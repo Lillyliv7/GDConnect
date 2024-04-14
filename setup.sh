da08745faa835a07e50f1c77a45671a35a40d7fe
Lilly <60449924+Lillyliv7@users.noreply.github.com>
2024-04-14 01:28:46 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 echo "GDConnect setup script\n"
 echo "Make sure you have Guilded and Discord bots with full permissions' tokens ready"
 echo "The Discord bot must also have \"Message Content Intent\" turned on in the Discord Developer Portal"
+echo "Press enter to continue"
+read nothing
 
 pip install python-dotenv
 pip install asyncio
